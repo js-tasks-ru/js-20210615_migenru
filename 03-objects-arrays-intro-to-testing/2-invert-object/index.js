b1f997f11697605a9fb24c5145506ca55d005e99
Mishchenko Evgeniy <migen.ru@gmail.com>
2021-06-24 17:16:54 +0300

    Done: 3 task - trim symbols

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,7 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  let result = {};
+  const result = {};
   if (typeof obj !== 'undefined') {
     for (const [key, value] of Object.entries(obj)) {
       result[value] = key;
